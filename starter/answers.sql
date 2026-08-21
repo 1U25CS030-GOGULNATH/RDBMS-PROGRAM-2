b1e9cf34b8ea7d14ee0d1a11a5957b752294e940
1U25CS030-GOGULNATH <gogul5809@gmail.com>
2026-08-21 12:21:52 +0530

    Update answers.sql

--- starter/answers.sql
+++ starter/answers.sql
@@ -1,5 +1,3 @@
-create database colleg;
-use colleg;
 
 create table student2(StudentID integer(5) primary key,Studentname varchar(20),DOB date unique,gender varchar(10),departmentID integer(10) not null);
 desc student2;
