69b1e75e3e54222fd7f4596e3982aa8e6f80bc62
1U25CS030-GOGULNATH <gogul5809@gmail.com>
2026-08-21 12:30:28 +0530

    Update answers.sql

--- starter/answers.sql
+++ starter/answers.sql
@@ -1,6 +1,8 @@
+create database cll;
+use cll;
 
 create table student2(StudentID integer(5) primary key,Studentname varchar(20),DOB date unique,gender varchar(10),departmentID integer(10) not null);
 desc student2;
 
-insert into student2 values ("12030","Gogul","2007-10-06","Male","10030");
+insert student2 values ("12030","Gogul","2007-10-06","Male","10030");
 select * from student2;
