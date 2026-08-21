5a85f977b0bf6a5b65fcdf5b328366f35c2d0dfd
1U25CS030-GOGULNATH <gogul5809@gmail.com>
2026-08-21 12:12:28 +0530

    Update answers.sql

--- starter/answers.sql
+++ starter/answers.sql
@@ -1,8 +1,8 @@
-create database colle;
-use colle;
+create database colleg;
+use colleg;
 
-create table student1(StudentID integer(5) primary key,Studentname varchar(20),DOB date unique,gender varchar(10),departmentID integer(5) not null);
-desc student1;
+create table student2(StudentID integer(5) primary key,Studentname varchar(20),DOB date unique,gender varchar(10),departmentID integer(10) not null);
+desc student2;
 
-insert into student1 values ("12030", "Gogul", "2007-10-06", "Male", "10030");
-select * from student1;
+insert into student2 values ("12030","Gogul","2007-10-06","Male","10030");
+select * from student2;
